af7a22def8abef74f8fb2d09590e355c4d65dc2f
Garrett Harkness <gharkness@sourceallies.com>
2024-12-14 10:45:48 -0600

    end of hibernate dao section

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -3,6 +3,7 @@ package guru.springframework.jdbc.dao;
 import guru.springframework.jdbc.domain.Author;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,27 +18,64 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author getById(Long id) {
-        return getEntityManager().find(Author.class, id);
+        EntityManager em = getEntityManager();
+        Author author = getEntityManager().find(Author.class, id);
+        em.close();
+        return author;
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        return null;
+        EntityManager em = getEntityManager();
+        TypedQuery<Author> query = getEntityManager().createQuery("SELECT a FROM Author a " +
+                "WHERE a.firstName = :first_name AND a.lastName = :last_name", Author.class);
+
+        query.setParameter("first_name", firstName);
+        query.setParameter("last_name", lastName);
+
+        Author author = query.getSingleResult();
+        em.close();
+        return author;
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        EntityManager em = getEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(author);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
+
+        return author;
     }
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        EntityManager em = getEntityManager();
+
+        try {
+            em.joinTransaction();
+            em.merge(author);
+            em.flush();
+            em.clear();
+            Author savedAuthor = em.find(Author.class, author.getId());
+            return savedAuthor;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void deleteAuthorById(Long id) {
-
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Author author = em.find(Author.class, id);
+        em.remove(author);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
     }
 
     private EntityManager getEntityManager() {
--- src/main/java/guru/springframework/jdbc/domain/Book.java
+++ src/main/java/guru/springframework/jdbc/domain/Book.java
@@ -5,10 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Book {
 
     @Id
